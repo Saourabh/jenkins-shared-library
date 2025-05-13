7adb6ee0f04c143da1a31285646798747c0e5092
Saourabh <saourabh17@gmail.com>
2025-05-13 15:29:40 +0530

    Update dockerpush.groovy

--- vars/dockerpush.groovy
+++ vars/dockerpush.groovy
@@ -1,14 +1,15 @@
-def call(String credentialsId, String imageName, String imageTag ){
+def call(String credentials, String imageName, String imageTag ){
   echo "Pushing Docker image: ${imageName}:${imageTag}"
     
     withCredentials([usernamePassword(
         credentialsId: credentials,
         usernameVariable: 'DOCKER_USERNAME',
         passwordVariable: 'DOCKER_PASSWORD')]) {
-        sh """
+        sh """""
             echo "\$DOCKER_PASSWORD" | docker login -u "\$DOCKER_USERNAME" --password-stdin
+            docker image tag ${imageName}:${imageTag} ${env.dockerHubUser}/${imageName}:${imageTag}
             docker push ${imageName}:${imageTag}
             docker push ${imageName}:latest
-        """
+        """""
     }
 }
